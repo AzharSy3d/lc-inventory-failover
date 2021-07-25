ffcbc81465792f67d366a8d18d7b53be6ec93271
AzharSy3d <azharuworld@gmail.com>
2021-07-26 00:18:14 +0530

    Initial Project Setup

---
+++ src/main/java/com/azsyed/lcinventoryfailover/LcInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcinventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcInventoryFailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcInventoryFailoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/azsyed/lcinventoryfailover/LcInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azsyed.lcinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
